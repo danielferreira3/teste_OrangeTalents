5f1c3513abe002ebddd340164ca3c79dfc998d76
danielferreira3 <daniel.z.ferreira@hotmail.com>
2021-08-04 11:32:21 -0300

    Documentação Api

--- controleVacinas/src/main/java/com/orange/controleVacinas/model/Usuario.java
+++ controleVacinas/src/main/java/com/orange/controleVacinas/model/Usuario.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
--- controleVacinas/src/main/java/com/orange/controleVacinas/repository/RepositoryUsuario.java
+++ controleVacinas/src/main/java/com/orange/controleVacinas/repository/RepositoryUsuario.java
@@ -3,11 +3,15 @@ package com.orange.controleVacinas.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.orange.controleVacinas.model.Usuario;
 
 @Repository
 public interface RepositoryUsuario extends JpaRepository<Usuario, Long> {
+	
 	public List <Usuario> findAllByNomeContainingIgnoreCase(String nome);
+	
+	
 }
